7cfea5caf405248a5f6df15e975c49d56dcac050
peter <peter.w@droidtown.co>
2021-06-25 06:34:56 +0800

    調整輸出結果的儲存方式為 mode=append

--- Loki_ScammerDisguise/UtteranceExtractor.py
+++ Loki_ScammerDisguise/UtteranceExtractor.py
@@ -18,42 +18,46 @@ if __name__ == "__main__":
 
     purgedPat = re.compile("</?[a-zA-Z]+?_?[a-zA-Z]*?>")
 
-    key_tokenLIST = ["偽裝", "欺騙", "喬裝", "假裝", "裝成", "佯裝"]
+    key_tokenLIST = ["偽裝", "冒充", "喬裝", "假裝", "假冒", "佯裝", "裝扮"]
     utteranceLIST = []
 
-    fileLIST = os.listdir("./臺灣澎湖地方法院")
+    fileLIST = os.listdir("./臺灣高等法院臺南分院")
     for f in fileLIST:
-        with open("./臺灣澎湖地方法院/{}".format(f)) as j:
+        print("{}/{} >> Processing: {}".format(fileLIST.index(f), len(fileLIST), f))
+        with open("./臺灣高等法院臺南分院/{}".format(f)) as j:
             jdict = json.loads(j.read())
 
-        text = jdict["judgement"].replace("\r\n", "").replace(" ", "")
-        if any(k in text for k in key_tokenLIST):
-            if len(text) >= 20000:
-                for i in range(len(text)-1, 0, -1): #倒著推算第一個不是中文字符的東西 (通常就是標點符號了啦)
-                    if not unicodedata.name(text[i]).startswith("CJK") and i < 20000:
-                        text = text[:i] #很隨便地只取了 2 萬字以內的完整句子。2 萬字以上就…算了。這一段只是草草洗個資料而已，重點是下一步的 Loki
-                        break 
-                    else: 
-                        pass
-            else:
-                pass
-
-            resultDICT = articut.parse(text)
-            while not isinstance(resultDICT, dict) or 'result_pos' not in resultDICT.keys():  
-                print(f'Unable to get results for {f}, wait 30 seconds.')
-                time.sleep(30)
-                resultDICT = articut.parse(text)
-
-            for r in resultDICT["result_pos"]:
-                if len(r) == 1:  #標點符號
-                    pass
+        if "詐欺" in jdict["reason"]:
+            text = jdict["judgement"].replace("\r\n", "").replace(" ", "")
+            if any(k in text for k in key_tokenLIST):
+                if len(text) >= 20000:
+                    for i in range(len(text)-1, 0, -1): #倒著推算第一個不是中文字符的東西 (通常就是標點符號了啦)
+                        try:
+                            if not unicodedata.name(text[i]).startswith("CJK") and i < 20000:
+                                text = text[:i] #很隨便地只取了 2 萬字以內的完整句子。2 萬字以上就…算了。這一段只是草草洗個資料而已，重點是下一步的 Loki
+                                break
+                            else:
+                                pass
+                        except:
+                            pass
                 else:
-                    sentence = re.sub(purgedPat, "", r)
-                    if any(k in sentence for k in key_tokenLIST):
-                        utteranceLIST.append(sentence)
+                    pass
 
+                resultDICT = articut.parse(text)
+                while not isinstance(resultDICT, dict) or 'result_pos' not in resultDICT.keys():
+                    print(f'Unable to get results for {f}, wait 30 seconds.')
+                    time.sleep(30)
+                    resultDICT = articut.parse(text)
 
-    with open("CollectedUtterance.json", "w", encoding='utf8') as j:
-        json.dump(utteranceLIST, j, ensure_ascii=False)
+                for r in resultDICT["result_pos"]:
+                    if len(r) == 1:  #標點符號
+                        pass
+                    else:
+                        sentence = re.sub(purgedPat, "", r)
+                        if any(k in sentence for k in key_tokenLIST):
+                            utteranceLIST.append(sentence)
+                            with open("CollectedUtterance.json", "a", encoding='utf8') as j:
+                                json.dump(utteranceLIST, j, ensure_ascii=False)
+                                utteranceLIST = []
 
 
